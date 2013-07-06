fb60b14d867ec5f00e338d4166183b151ab9e8c9
Cody P Schafer <devel@codyps.com>
2013-07-06 07:47:19 -0700

    ups.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -9,25 +9,14 @@ flags = [
 '-Wall',
 '-Wextra',
 '-Werror',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
+# Determines header file lang
 '-std=gnu11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'-I',
-'.',
-'-I',
-'ccan'
+# Source file lang
+'-x', 'c',
+# Includes
+'-I', '.',
+'-I', 'ccan',
+'-I', 'penny'
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
